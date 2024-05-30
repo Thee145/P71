65cd815bb9c3e89e0c982165db8de26d5db4a25c
Thee145 <117292950+Thee145@users.noreply.github.com>
2024-05-30 12:03:13 -0400

    Add files via upload

---
+++ P71/PRO-C71-Template-master/config.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyAiZ0rk0eJLqyV76G9guOCv_bjMDk0IAeI",
+    authDomain: "e-ride-fcd98.firebaseapp.com",
+    databaseURL: "https://e-ride-fcd98-default-rtdb.firebaseio.com",
+    projectId: "e-ride-fcd98",
+    storageBucket: "e-ride-fcd98.appspot.com",
+    messagingSenderId: "810824521645",
+    appId: "1:810824521645:web:3b28aae769306548027324"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
